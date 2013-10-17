01e5a95f076d0ca089d8392c5bcd11fac970a30f
Leonard Tulipan <knowhowbites@gmail.com>
2013-10-17 14:43:24 +0100

    Adding Plugins in Dev Version to repository/branch

--- wp-config.php
+++ wp-config.php
@@ -1,24 +1,24 @@
 <?php
 /**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+* The base configurations of the WordPress.
+*
+* This file has the following configurations: MySQL settings, Table Prefix,
+* Secret Keys, WordPress Language, and ABSPATH. You can find more information
+* by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+* wp-config.php} Codex page. You can get the MySQL settings from your web host.
+*
+* This file is used by the wp-config.php creation script during the
+* installation. You don't have to use the web site, you can just copy this file
+* to "wp-config.php" and fill in the values.
+*
+* @package WordPress
+*/
 
 // ** Heroku Postgres settings - from Heroku Environment ** //
 if (isset($_ENV["DATABASE_URL"]))
-  $db = parse_url($_ENV["DATABASE_URL"]);
+$db = parse_url($_ENV["DATABASE_URL"]);
 else
-  $db = parse_url($_SERVER["DATABASE_URL"]);
+$db = parse_url($_SERVER["DATABASE_URL"]);
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -40,14 +40,14 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 /**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+* Authentication Unique Keys and Salts.
+*
+* Change these to different unique phrases!
+* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+*
+* @since 2.6.0
+*/
 define('AUTH_KEY',         'fgo|(F6g5/P)Y-t43zssjd9w9wbbC2l!CwsHAQFnU{q2F XN=z:4yCl,H.69a$d]');
 define('SECURE_AUTH_KEY',  'ThSxmPwNMCjIq_+uJ:3Lo4X?zg_3n-0feenmhehb00JH%L^O8QYy-.rT2q!(hA,v');
 define('LOGGED_IN_KEY',    'iZ]w)t/g!8)|{%nH<> 6|d`8a@a<ENm+dhjkhj*`JN~LPu!JC:;kp*1^cpQv6Oh_');
@@ -60,37 +60,37 @@ define('NONCE_SALT',       'E%wQA1p^;|anOCFIk>yIwTm3wS8DzWmWL>IC8fB/M*Therk#}c2x
 /**#@-*/
 
 /**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+* WordPress Database Table prefix.
+*
+* You can have multiple installations in one database if you give each a unique
+* prefix. Only numbers, letters, and underscores please!
+*/
 $table_prefix  = 'wp_';
 
 /**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
+* WordPress Localized Language, defaults to English.
+*
+* Change this to localize WordPress. A corresponding MO file for the chosen
+* language must be installed to wp-content/languages. For example, install
+* de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+* language support.
+*/
 define('WPLANG', '');
 
 /**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
+* For developers: WordPress debugging mode.
+*
+* Change this to true to enable the display of notices during development.
+* It is strongly recommended that plugin and theme developers use WP_DEBUG
+* in their development environments.
+*/
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
