e45d73811ad4eaa1c4e5fc716698b63b050dd2e7
Leonard Tulipan <knowhowbites@gmail.com>
2013-11-22 11:58:26 +0000

    Added date check for latest.dump in case of trouble with curl download

--- db.from.heroku.sh
+++ db.from.heroku.sh
@@ -1,4 +1,6 @@
 sudo heroku pgbackups:capture -a leichtergesundleben --expire
 curl -o latest.dump `heroku pgbackups:url`
+echo "Date of latest.dump:"
+ ls --time-style full-iso -l latest.dump |awk '{print $6}'
 echo "Now run: "
 echo "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U wordpress -d wordpress latest.dump"
