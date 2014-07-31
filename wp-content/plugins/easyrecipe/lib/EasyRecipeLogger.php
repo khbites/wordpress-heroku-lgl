a9c864121f0815f7bd32c052893c49e04b1aa2fc
Leonard Tulipan <knowhowbites@gmail.com>
2014-07-31 17:14:01 +0100

    Added EasyRecipe plugin

---
+++ wp-content/plugins/easyrecipe/lib/EasyRecipeLogger.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Logging module
+ *
+ * Relies on EasyLogger being installed and active - does nothing if it's not
+ */
+/**
+ * If EasyLogger isn't installed, create stub classes
+ */
+if (!class_exists('EasyLogger', false)) {
+    class EasyRecipeLogger {
+        static function getLog($logfile) {
+            return new EasyLoggerLog();
+        }
+    }
+
+    if (!class_exists('EasyLoggerLog', false)) {
+        class EasyLoggerLog {
+
+            function comment($msg) {
+            }
+
+            function disable($level) {
+            }
+
+            function enable($level) {
+            }
+
+            function debug($msg) {
+            }
+
+            function info($msg) {
+            }
+
+            function warn($msg) {
+            }
+
+            function error($msg) {
+            }
+
+            function fatal($msg) {
+            }
+        }
+    }
+} else {
+    /**
+     * Class EasyRecipeLogger
+     *
+     * Use the real EasyLogger
+     */
+    class EasyRecipeLogger {
+        /**
+         * @param string $logfile
+         * @return EasyLoggerLog
+         */
+        static function getLog($logfile) {
+            return EasyLogger::getLog($logfile);
+        }
+    }
+
+}
+
